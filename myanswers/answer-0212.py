78b5af439a8d4236e0058a4991e0b8c73fc21193
Catalina1101 <catalina.guerra@udea.edu.co>
2026-05-31 15:03:39 -0500

    Convert PCA output to list format
    Convert transformed PCA output to list to avoid ValueError.

--- myanswers/answer-0212.py
+++ myanswers/answer-0212.py
@@ -2,7 +2,8 @@ import numpy as np
 from sklearn.decomposition import PCA
 
 def reducir_dimensionalidad_pca(X):
-
     pca = PCA(n_components=2)
     X_transformada = pca.fit_transform(X)
-    return X_transformada
+    
+    # Al convertirlo a lista, evitas el ValueError
+    return X_transformada.tolist()
