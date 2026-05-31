cf801696a89da0f1763ab4b2863167e0d429b052
Catalina1101 <catalina.guerra@udea.edu.co>
2026-05-31 14:07:08 -0500

    Remove docstring from PCA dimensionality reduction function
    Removed docstring from reducir_dimensionalidad_pca function.

--- myanswers/answer-0212.py
+++ myanswers/answer-0212.py
@@ -2,15 +2,7 @@ import numpy as np
 from sklearn.decomposition import PCA
 
 def reducir_dimensionalidad_pca(X):
-    """
-    Reduce la dimensionalidad de X a 2 componentes principales usando PCA.
-    
-    Parámetros:
-        X (np.ndarray): Matriz de datos de forma (n_muestras, n_features)
-    
-    Retorna:
-        np.ndarray: Matriz transformada de forma (n_muestras, 2)
-    """
+
     pca = PCA(n_components=2)
     X_transformada = pca.fit_transform(X)
     return X_transformada
