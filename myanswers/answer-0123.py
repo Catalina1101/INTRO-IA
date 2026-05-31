cfa221b38f653da1c86e8aa3dc430f06b2e678fa
Catalina1101 <catalina.guerra@udea.edu.co>
2026-05-31 12:58:26 -0500

    Implement PCA for dimensionality reduction
    Added a function to reduce data dimensionality using PCA.

--- myanswers/answer-0123.py
+++ myanswers/answer-0123.py
@@ -1,1 +1,17 @@
+from sklearn.decomposition import PCA
 
+def reducir_dimensionalidad_pca(X):
+    """
+    Reduce una matriz de datos a 2 componentes principales usando PCA.
+    
+    Parámetros
+    X : array-like
+        Matriz de datos de entrada.
+
+    Retorna
+    np.ndarray
+        Matriz transformada con 2 dimensiones.
+    """
+    pca = PCA(n_components=2)
+    X_reducido = pca.fit_transform(X)
+    return X_reducido
